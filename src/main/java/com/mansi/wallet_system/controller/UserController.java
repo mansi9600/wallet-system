df4b43b23455fc9bb8a6413149297fc7f100abcd
Mansi Gupta <mansiguptamg44@gmail.com>
2026-07-17 00:30:40 +0530

    Add UserController

---
+++ src/main/java/com/mansi/wallet_system/controller/UserController.java
@@ -0,0 +1,28 @@
+package com.mansi.wallet_system.controller;
+
+import com.mansi.wallet_system.entity.User;
+import com.mansi.wallet_system.service.UserService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping
+    public User createUser(@RequestBody User user) {
+        return userService.saveUser(user);
+    }
+
+    @GetMapping
+    public List<User> getAllUsers() {
+        return userService.getAllUsers();
+    }
+}
