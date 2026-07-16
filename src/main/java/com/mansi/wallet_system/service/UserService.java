b01872ede819418582bb089312ea87146548c4c4
Mansi Gupta <mansiguptamg44@gmail.com>
2026-07-17 00:26:50 +0530

    Add UserService

---
+++ src/main/java/com/mansi/wallet_system/service/UserService.java
@@ -0,0 +1,26 @@
+
+package com.mansi.wallet_system.service;
+
+import com.mansi.wallet_system.entity.User;
+import com.mansi.wallet_system.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public User saveUser(User user) {
+        return userRepository.save(user);
+    }
+
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+}
