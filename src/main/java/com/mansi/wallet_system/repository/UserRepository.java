27dd81fa6e2c5470d988747fb0f399befb58890f
Mansi Gupta <mansiguptamg44@gmail.com>
2026-07-17 00:22:36 +0530

    Add UserRepository

---
+++ src/main/java/com/mansi/wallet_system/repository/UserRepository.java
@@ -0,0 +1,11 @@
+
+package com.mansi.wallet_system.repository;
+
+import com.mansi.wallet_system.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
